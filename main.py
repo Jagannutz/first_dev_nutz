da838653dc0b17e8bb3f6c2d53af89c5b88456cc
Jagannutz <jagansengottaian333@gmail.com>
2025-07-15 10:24:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(f"hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/') # This defines the route for the root URL
+def home_page():
+    return "Hello Gopi"
+
+@app.route('/Jagan') # This defines the route for the root URL
+def hello():
+    return "Hello Jagan"
+
+# ... other routes you might have ...
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
